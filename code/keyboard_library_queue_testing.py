b4dbf919199678d84420cd9217aa2ec0992f88f6
Esteban Serrano R <e.serrano.r@me.com>
2013-03-15 01:30:41 -0400

    modificada la libreria para que cada proc tenga un lector independiente

--- code/keyboard_library_queue_testing.py
+++ code/keyboard_library_queue_testing.py
@@ -2,6 +2,12 @@
 import keyboard_library_queue
 import sys
 
+
+def print_event(sender, earg):
+	print str(earg)
+	
+
 lib = keyboard_library_queue.KeyboardLibrary()
+lib.keypress += print_event
 lib.run(0x0e8f,0x0022)
 

b95008047689c5dc0caac42e515f8067727a972d
Andrea Vasquez <avasquez@gmail.com>
2013-03-26 18:03:18 -0400

    Ordenando el codigo de Eustaquio

--- code/audio_library_2.py
+++ code/audio_library_2.py
@@ -3,21 +3,17 @@ import usb.util
 
 class AudioLibrary:
 
+	tarjetas = []
 
+	#Obtener VendorID y ProductID de una forma inteligente
+	#Hardcodded por ahora
 
-	"""
-	>>> import usb.core
-	>>> import usb.util
-	>>> tarjetas = []
-	>>> tarjetas.append(usb.core.find(idVendor=0x0d8c))
-	>>> tarjetas.append(usb.core.find(idVendor=0x0c76))
-	>>> tarjetas.append(usb.core.find(idVendor=0x1130))
-	>>> len(tarjetas)
-	3
-	>>> for t in tarjetas:
-	...     t.detach_kernel_driver(0)
-	... 
-	>>> 
-	>>> for t in tarjetas:
-	...     t.attach_kernel_driver(0)
-	"""
+	tarjetas.append(usb.core.find(idVendor=0x0d8c))
+	tarjetas.append(usb.core.find(idVendor=0x0c76))
+	tarjetas.append(usb.core.find(idVendor=0x1130))
+
+	def __init__(self):
+		for t in tarjetas:
+			t.detach_kernel_driver(0)
+
+	#Llamar t.attach_kernel_driver(0) para retomar control

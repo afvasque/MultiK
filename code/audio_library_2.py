ff07dfcde090c30f1b1542dd7940ef89796cc11f
Andrea Vasquez <avasquez@gmail.com>
2013-03-28 12:24:04 -0400

    Deattaching cards from kernel

--- code/audio_library_2.py
+++ code/audio_library_2.py
@@ -1,19 +1,30 @@
 import usb.core
 import usb.util
+import itertools
+import os
 
 class AudioLibrary:
 
+	tarjetas1 = []
+	tarjetas2 = []
+	tarjetas3 = []
 	tarjetas = []
 
 	#Obtener VendorID y ProductID de una forma inteligente
 	#Hardcodded por ahora
 
-	tarjetas.append(usb.core.find(idVendor=0x0d8c))
-	tarjetas.append(usb.core.find(idVendor=0x0c76))
-	tarjetas.append(usb.core.find(idVendor=0x1130))
+	tarjetas1 = usb.core.find(find_all = True, idVendor=0x0d8c)
+	tarjetas2 = usb.core.find(find_all = True, idVendor=0x0c76)
+	tarjetas3 = usb.core.find(find_all = True, idVendor=0x1130)
 
 	def __init__(self):
-		for t in tarjetas:
-			t.detach_kernel_driver(0)
+		for counter, t in enumerate(itertools.chain(self.tarjetas1, self.tarjetas2, self.tarjetas3)):
+			print "Tarjeta %s" % counter
+			print str(t.bus)
+			print str(t.address)
+			self.tarjetas.append(t)
+			if t.is_kernel_driver_active(0):
+				t.detach_kernel_driver(0)
+		os.system("aplay -l")
 
 	#Llamar t.attach_kernel_driver(0) para retomar control
---
+++ code/audio_library_2_test.py
@@ -0,0 +1,4 @@
+# coding=utf-8
+import audio_library_2
+
+lib = audio_library_2.AudioLibrary()

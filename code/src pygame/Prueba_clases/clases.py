83dfa0017441a988eb41888131bea7becdf9b0d6
Tomas Martinez <timartin@uc.cl>
2013-05-14 12:12:11 -0400

    arreglado el textbox de enzo

--- code/src pygame/Prueba_clases/clases.py
+++ code/src pygame/Prueba_clases/clases.py
@@ -2,23 +2,24 @@ import pygame
 
 
 class Textbox:
-	def __init__(self,canvas, pos_x, pos_y, width, height):
+	def __init__(self, pos_x, pos_y, width, height):
 		self.pos_x = pos_x
 		self.pos_y = pos_y
 		self.Color = pygame.Color(0,255,0)
-		self.whiteColor= pygame.Color(255,255,255)
+		self.whiteColor = pygame.Color(255,255,255)
+		self.blackColor = pygame.Color(0,0,0)
 		self.width = width
 		self.height = height
-		self.canvas=canvas
+		self.canvas = pygame.Surface((self.width,self.height))
+		self.canvas.fill(self.blackColor)
+		pygame.draw.rect(self.canvas,self.whiteColor,(1, 1, self.width - 2, self.height - 2))
 		#Sector especifico para el inicio
 		
 		self.Value = ""
 		
-		pygame.draw.rect(self.canvas,self.Color,(self.pos_x, self.pos_y, self.width, self.height))
-		
 		pygame.font.init()
 		
-		self.myfont = pygame.font.SysFont("monospace", self.height)
+		self.myfont = pygame.font.SysFont("monospace", self.height - 2)
 
 	def screen(self):
 		return self.canvas
@@ -26,19 +27,19 @@ class Textbox:
 	def react(self,input):
 		if len(input)==1:
 			self.Value = self.Value + input
-			print "valor: "+self.Value
+			print "valor: " + self.Value
 
-			self.myfont = pygame.font.SysFont("monospace", self.height)
-			label = self.myfont.render(self.Value, 1, (0,0,0))
-			self.canvas.blit(label,(self.pos_x, self.pos_y))
+			self.myfont = pygame.font.SysFont("monospace", self.height - 2)
+			label = self.myfont.render(self.Value, 1, self.blackColor)
+			self.canvas.blit(label,(1, 1))
 
 
 		elif input == "Back":
 			if len(self.Value) > 0:
-				pygame.draw.rect(self.canvas,self.Color,(self.pos_x, self.pos_y, self.width, self.height))
+				pygame.draw.rect(self.canvas,self.whiteColor,(1, 1, self.width - 2, self.height - 2))
 				self.Value = self.Value[:-1]
-				self.myfont = pygame.font.SysFont("monospace", self.height)
-				label = self.myfont.render(self.Value, 1, (0,0,0))
-				self.canvas.blit(label,(self.pos_x, self.pos_y))
+				self.myfont = pygame.font.SysFont("monospace", self.height - 2)
+				label = self.myfont.render(self.Value, 1, self.blackColor)
+				self.canvas.blit(label,(1, 1))
 		return
 				

7c3d9207cb28fc1ccab7a4e60aeb5d373788bc2b
Andrea Vasquez <avasquez@gmail.com>
2013-05-14 12:59:32 -0300

    corrigiendo error en lectura y display de numero

--- code/src pygame/Prueba_clases/ejercicio.py
+++ code/src pygame/Prueba_clases/ejercicio.py
@@ -234,7 +234,7 @@ class ejercicio:
 			self.lib_play_proc.start()          
 		
 		if len(text_to_speech)>0:
-			print "Reproduciendo en audÃ­fono #%s: \"%s\"" % (self.numero_audifono, text_to_speech)
+			print "Reproduciendo en audífono #%s: \"%s\"" % (self.numero_audifono, text_to_speech)
 			audio_lib.reproduciendo[int(self.numero_audifono)]=True
 			print str(self.numero_audifono)+" "+str(audio_lib.reproduciendo[self.numero_audifono])
 			self.text_to_speech_queue.put(text_to_speech)
@@ -246,7 +246,7 @@ class ejercicio:
 
 		self.ResetLayout()
 
-		self.Operacion_actual.audio_pregunta= "Escribe el número"+str(self.numero_audifono)
+		self.Operacion_actual.audio_pregunta= "Escribe el número %d" % self.numero_audifono
 
 		self.TexttoSpeech(self.Operacion_actual.audio_pregunta)
 
@@ -314,6 +314,122 @@ class ejercicio:
 		
 		
 		'''
+
+
+	def Keyboard_Pressed(self, sender, earg):
+
+		text= str(earg['char']).decode('utf-8')
+		text= self.arreglar_texto(text)
+		
+		if len(text)==0:
+			return
+
+		if text == "Enter":
+			
+			if ((self.Operacion_actual.TipoOperacion == TipoOperacion.Reproduccion_letras_alfabeto and self.Operacion_actual.nivelOperacion == 2) or
+				(self.Operacion_actual.TipoOperacion == TipoOperacion.mayus_nombres_propios and self.Operacion_actual.nivelOperacion == 1) or
+				(self.Operacion_actual.TipoOperacion == TipoOperacion.patrones_ort_comunes and self.Operacion_actual.nivelOperacion == 1) or
+				(self.Operacion_actual.TipoOperacion == TipoOperacion.patrones_ort_comunes and self.Operacion_actual.nivelOperacion == 3) or
+				(self.Operacion_actual.TipoOperacion == TipoOperacion.patrones_ort_comunes and self.Operacion_actual.nivelOperacion == 4) or
+				(self.Operacion_actual.TipoOperacion == TipoOperacion.sentido_vocales_silabas)):
+				
+				if isinstance(self.box_left.GetChildren()[1].GetWindow(),wx.ListBox):
+					
+					list= self.box_left.GetChildren()[1].GetWindow()
+					if len(list.GetSelections())>0:
+						if list.Items[list.GetSelections()[0]] == self.Operacion_actual.respuesta:
+							list.Clear()
+							list.Append(self.Operacion_actual.feedback_correcto)
+							self.TexttoSpeech(self.Operacion_actual.feedback_correcto)
+							self.Operacion_actual.RespuestaCorrecta()
+
+						else:
+							list.Clear()
+							list.Append(self.Operacion_actual.feedback_error)
+							self.TexttoSpeech(self.Operacion_actual.feedback_error)
+							self.Operacion_actual.RespuestaIncorrecta()
+			
+			elif ((self.Operacion_actual.TipoOperacion == TipoOperacion.Reproduccion_letras_alfabeto and self.Operacion_actual.nivelOperacion == 1) or
+				(self.Operacion_actual.TipoOperacion == TipoOperacion.signos_int_excl and self.Operacion_actual.nivelOperacion == 1) or
+				(self.Operacion_actual.TipoOperacion == TipoOperacion.patrones_ort_comunes and self.Operacion_actual.nivelOperacion == 2) or
+				(self.Operacion_actual.TipoOperacion == TipoOperacion.patrones_ort_comunes and self.Operacion_actual.nivelOperacion == 5)):
+				
+				#textctrl= wx.TextCtrl
+				textctrl= self.box_left.GetChildren()[1].GetWindow()
+				
+				if self.Operacion_actual.TipoOperacion == TipoOperacion.signos_int_excl:
+					resp= self.Operacion_actual.respuesta.split(",")
+					if ((resp[0] in textctrl.Value) and (resp[1] in textctrl.Value)):
+						textctrl.Value= self.Operacion_actual.respuesta
+						
+				if textctrl.Value == self.Operacion_actual.respuesta:
+					self.TexttoSpeech(self.Operacion_actual.feedback_correcto)
+					self.Operacion_actual.RespuestaCorrecta()
+					textctrl.Value=""
+				else:
+					self.TexttoSpeech(self.Operacion_actual.feedback_error)
+					self.Operacion_actual.RespuestaIncorrecta()
+					textctrl.Value=""
+					
+			self.Operacion_actual= self.reglas_main.GetSiguienteOperacion(self.Operacion_actual, self.Alumno_actual)
+			wx.CallAfter(self.CreateGrid,self.Operacion_actual)
+		   
+
+		#if self.box_left.GetChildren()[1].GetWindow().Value == None:
+		#    self.box_left.GetChildren()[1].GetWindow().SetValue('')
+
+
+		if isinstance(self.box_left.GetChildren()[1].GetWindow(),wx.ListBox):
+			if text == "-v" or text == "-^":
+
+				if text == "-v":
+
+					if len(self.box_left.GetChildren()[1].GetWindow().GetSelections()) ==0:
+						self.box_left.GetChildren()[1].GetWindow().Select(0)
+						
+					else:
+						num=self.box_left.GetChildren()[1].GetWindow().GetSelections()[0]
+
+						if num == self.box_left.GetChildren()[1].GetWindow().GetCount()-1:
+							self.box_left.GetChildren()[1].GetWindow().Select(0)
+						else:
+							self.box_left.GetChildren()[1].GetWindow().Select(num+1)
+
+				elif text == "-^":
+					if len(self.box_left.GetChildren()[1].GetWindow().GetSelections()) ==0:
+						cont=self.box_left.GetChildren()[1].GetWindow().GetCount()-1
+						self.box_left.GetChildren()[1].GetWindow().Select(cont)
+					else:
+						num=self.box_left.GetChildren()[1].GetWindow().GetSelections()[0]
+
+						if num==0:
+							cont=self.box_left.GetChildren()[1].GetWindow().GetCount()-1
+							self.box_left.GetChildren()[1].GetWindow().Select(cont)
+						else:
+							self.box_left.GetChildren()[1].GetWindow().Select(num-1)
+
+
+		try:
+			
+			if text == "Back": # backspace captura
+				self.box_left.GetChildren()[1].GetWindow().Value=self.box_left.GetChildren()[1].GetWindow().Value[:-1]
+				return
+
+			if isinstance(self.box_left.GetChildren()[1].GetWindow(),wx.TextCtrl):
+				strr= self.box_left.GetChildren()[1].GetWindow().Value
+				#wx.CallAfter(self.box_left.GetChildren()[1].GetWindow().SetValue(strr+text))
+				self.box_left.GetChildren()[1].GetWindow().Value=strr+text
+
+
+
+		except TypeError as e:
+			print "Error: %s" % str(e)
+		except UnicodeDecodeError as e:
+			print "Error: %s" % str(e)
+		except:
+			print "Unexpected error:"#, sys.exc_info()[0]
+
+
 
 	def set_nombre(self):
 		self.ResetLayout()

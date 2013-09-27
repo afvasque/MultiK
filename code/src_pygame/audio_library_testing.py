d49a252144e5eca428cc9d6c3cff68b5326e86c6
Esteban Serrano R <e.serrano.r@me.com>
2013-09-27 17:07:43 -0300

    cambios en lib de audio para llamar con lib.play(id_alumno, text_to_speech). ademas cambio en audio_library_testing para ejemplificar

--- code/src_pygame/audio_library_testing.py
+++ code/src_pygame/audio_library_testing.py
@@ -6,66 +6,21 @@ import multiprocessing
 class LibTest:
 	lib = audio_library.AudioLibrary()
 
-	all_p = []
-	all_q = []
-
-
-	def imprimir_algo_en_pantalla(self, sender, earg):
+	def print_finished_event(self, sender, earg):
 		print "Terminó audio. Datos: " + str(earg)
 
-	def terminate_proc(self, sender, earg):
-		if earg['terminate']:
-			print "Terminando proceso %s..." % str(self.all_p[earg['id']])
-			try:
-				self.all_p[earg['id']].terminate()
-			except AttributeError as e:
-				print "AttributeError: %s" % (e)
-				pass
-
-
 	def __init__(self):
-		self.lib.finished += self.imprimir_algo_en_pantalla
-		self.lib.finished += self.terminate_proc
+		self.lib.finished += self.print_finished_event
 
 
 		for i in range(0,self.lib.get_total_usb_cards()):
-			# get a text from the queue
-			text_to_speech_queue = multiprocessing.Queue()
-			# create a process for each card
-			p = multiprocessing.Process(target=self.lib.play, args=(i, text_to_speech_queue))
-			# set as daemon
-			p.daemon = True	
-			# append to the process array
-			self.all_p.append(p)
-			# start it
-			p.start()
-
-			# append the queue to the queue array
-			self.all_q.append(text_to_speech_queue)
-
 			# text to play
-			text_to_speech = "Hola. Tarjeta %d. Chao." % i
-			tts_id = i # random id, used to identify the finished event
-			# put the text into the corresponding queue
-			text_to_speech_queue.put({'tts': text_to_speech, 'terminate': False, 'tts_id': tts_id})
+			text_to_speech = "En Cocoyoc abundan los perros pug, querido alumno %d." % i
+			# tell the lib to play the tts
+			self.lib.play(i, text_to_speech)
 
 
-		for idx, queue in enumerate(self.all_q):
-			tts_id = idx + 1000
-			text_to_speech = "Hola. Tarjeta %d. Chao." % idx
-			queue.put({'tts': text_to_speech, 'terminate': False, 'tts_id': tts_id}) # True para terminar el proceso cuando termine de reproducir el audio
+		time.sleep(10)
 
-
-		# Iterar de nuevo por todas las colas de tarjetas y terminar los procesos cuando terminen
-		for idx, queue in enumerate(self.all_q):
-			tts_id = idx + 1500
-			#text_to_speech = "Hola de nuevo, tarjeta %d." % idx
-			text_to_speech = "Hola. Tarjeta %d. Chao." % idx
-			queue.put({'tts': text_to_speech, 'terminate': True, 'tts_id': tts_id}) # True para terminar el proceso cuando termine de reproducir el audio
-
-
-
-		for p in self.all_p:
-			p.join()
 
 LibTest()

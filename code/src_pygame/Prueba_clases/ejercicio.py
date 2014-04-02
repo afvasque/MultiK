67e17cd395af36fbdcce2c04b461152c733b0142
Pucca MultiK <avasquezg@gmail.com>
2014-04-02 15:25:17 -0400

    Colores mas suaves background ejercicios

--- code/src_pygame/Prueba_clases/ejercicio.py
+++ code/src_pygame/Prueba_clases/ejercicio.py
@@ -43,7 +43,7 @@ class ejercicio:
 		print "ejercicio"
 		self.pos_x = pos_x
 		self.pos_y = pos_y
-		self.whiteColor = pygame.Color(255,255,255)
+		self.whiteColor = pygame.Color(255,255,255, 100)
 		self.backColor = pygame.Color(random.randint(100, 255),random.randint(100, 255),random.randint(100, 255))
 		self.width = width
 		self.height = height

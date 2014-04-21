f4c84e38590a2390a44e4fe24f0e7226f6a2680b
Andrea Vasquez G <avasquezg@gmail.com>
2014-04-21 18:38:03 -0300

    Nueva libreria en colaborativo

--- code/Tomas/App/App1.py
+++ code/Tomas/App/App1.py
@@ -220,10 +220,6 @@ def Keyboard_event(sender, earg):
 			window.blit(Setups[alumno.id].screen(),(Setups[alumno.id].width * Setups[alumno.id].pos_x, Setups[alumno.id].height * Setups[alumno.id].pos_y))
 	pygame.display.flip()
 
-class ThreadKeyboard(threading.Thread):
-    def run(self):
-        lib.run([[0x0e8f,0x0022],[0x0e6a,0x6001]])
-
 class PygameThread(threading.Thread):    
     def run(self):
         clock = pygame.time.Clock()
@@ -242,54 +238,10 @@ class PygameThread(threading.Thread):
 lib.keypress += Keyboard_event
 
 try:
-    t = ThreadKeyboard()
-    t.start() 
 
     pygame_thread = PygameThread()
     pygame_thread.start()
 except:
     print("-----===== EXCEPTION threading exception =====-----")
 
-#lib.run(0x0e8f,0x0022)
-		
-'''
-for i in range(num_grupos):
-    teclados_grupo = []
-    if num_teclados >= 3 * i + 2:
-        teclados_grupo = [3 * i, 3 * i + 1, 3 * i + 2]
-    elif num_teclados == 3 * i + 1:
-        teclados_grupo = [3 * i, 3 * i + 1]
-    else:
-        teclados_grupo = [3 * i]
-    ejercicios.append(ejercicio1(teclados_grupo,i%line_number_x,i/line_number_x,width/line_number_x,height/line_number_y))
-    window.blit(ejercicios[i].screen(),(ejercicios[i].width *ejercicios[i].pos_x,ejercicios[i].height *ejercicios[i].pos_y))
-
-pygame.display.flip()
-
-def Keyboard_event(sender, earg):
-    print "#%s : %s" % (earg['id'], earg['char'])  # 0: id, 1: teclas
-    grupo = int(int(earg['id'])/3)
-    ejercicios[grupo].react(int(earg['id']), earg['char'])
-    if(ejercicios[grupo].finished):
-        ejercicios[grupo] = ejercicio2(ejercicios[grupo].teclados, ejercicios[grupo].pos_x, ejercicios[grupo].pos_y, ejercicios[grupo].width, ejercicios[grupo].height)
-    window.blit(ejercicios[grupo].screen(),(ejercicios[grupo].width *ejercicios[grupo].pos_x,ejercicios[grupo].height *ejercicios[grupo].pos_y))
-    pygame.display.flip()
-
-def close():
-	while True:
-		for event in pygame.event.get():
-			if event.type == QUIT:
-				pygame.quit()
-				sys.exit()
-
-Thread(target=close).start()
-
-lib.keypress += Keyboard_event
-
-lib.run(0x0e8f,0x0022)
-
-#while True:
-#    for event in pygame.event.get(): 
-#        if event.type == pygame.QUIT: 
-#            sys.exit(0)
-'''
+lib.run()
--- code/Tomas/App/keyboard_library_queue.py
+++ code/Tomas/App/keyboard_library_queue.py
@@ -7,10 +7,158 @@ import time
 import time
 import logging
 
-import InputDeviceDispatcher
+# Evdev library: http://python-evdev.readthedocs.org/en/latest/tutorial.html
+from evdev import InputDevice, categorize, ecodes
+
+from select import select
+from threading import Thread
+from asyncore import file_dispatcher, loop
+import string
 
 import re
 from subprocess import check_output
+
+
+# Avoids while True for reading input
+class InputDeviceDispatcher(file_dispatcher):
+	
+	diaeresis = False
+	acute = False
+	is_leftshift = False
+
+	keypress = event.Event('A key has been pressed')
+
+	vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+	vowels_acute = ['á', 'é', 'í', 'ó', 'ú', 'Á', 'É', 'Í', 'Ó', 'Ú']
+	vowels_diaeresis = ['ä', 'ë', 'ï', 'ö', 'ü', 'Ä', 'Ë', 'Ï', 'Ö', 'Ü']
+
+	def __init__(self, device):
+		self.device = device
+		file_dispatcher.__init__(self, device)
+
+	def recv(self, ign=None):
+		return self.device.read()
+
+	def handle_read(self):		
+		
+		time_pressed = time.time()
+
+		for event in self.recv():
+			if event.value == 1:
+				path = self.device.fn
+				
+				# Deletes /dev/input/event before ID
+				device_id = path[(path.find("event")+5):]	
+				
+				keycode = ecodes.KEY[event.code]
+				result = self.get_key_to_screen(keycode)
+				
+				if "KEY_" in result:
+					result = self.get_punctuation_marks(event.code)
+				
+				if result != "":
+					print([device_id,result])
+					values = {"id": device_id, "char": result, "time_pressed": time_pressed}
+					logging.info("[%f: [%d, %s, '%s'] ], " % (time_pressed, int(device_id), 'KEYPRESS', result))
+					self.keypress(values)
+
+	# Receives eventcode instead of keycode to avoid mapping mess. Eventcode is the same
+	# no matter the active keyboard layout
+	def get_punctuation_marks(self, eventcode):
+		if eventcode == 12:
+			if self.is_leftshift:
+				self.is_leftshift = False
+				return "?"
+		elif eventcode == 13:
+			if self.is_leftshift:
+				self.is_leftshift = False
+				return "¿"
+			else:
+				return "¡"
+		elif eventcode == 14:
+			return 'Back'
+		elif eventcode == 28:
+			return 'Enter'
+		elif eventcode == 103:
+			return '-^'
+		elif eventcode == 108:
+			return '-v'
+		elif eventcode == 105:
+			return '<-'
+		elif eventcode == 106:
+			return '->'
+		elif eventcode == 127:
+			return 'Pow'
+		elif eventcode == 39:
+			if self.is_leftshift:
+				self.is_leftshift = False
+				return 'Ñ'
+			else:
+				return 'ñ'
+		else:
+			return eventcode
+			
+
+	def get_key_to_screen(self, keycode):
+
+		# Deletes KEY_
+		key = keycode[keycode.find("KEY_")+4:] 
+		
+		# If is a letter
+		if key in string.ascii_letters:
+
+			if not self.is_leftshift:
+				key = key.lower()
+
+			if key in self.vowels:
+				vowel_pos = self.vowels.index(key)
+				if self.acute:
+					return self.vowels_acute[vowel_pos]
+				elif self.diaeresis:
+					return self.vowels_diaeresis[vowel_pos]
+
+			self.is_leftshift = False
+			self.acute = False
+
+			return key
+
+		# If key is a number
+		elif key in string.digits:
+			if key == "1" and self.is_leftshift:
+				self.is_leftshift = False
+				return "!"
+			return key
+
+		# Uppercase
+		elif keycode == "KEY_LEFTSHIFT":
+			self.is_leftshift = True
+			return ''
+		# Acute
+		elif keycode == "KEY_APOSTROPHE":
+			# Diaeresis
+			if self.is_leftshift:
+				self.acute = False
+				self.is_leftshift = False
+				self.diaeresis = True
+			else:
+				self.acute = True
+			return ''
+
+		elif keycode == "KEY_DOT":
+			if self.is_leftshift:
+				self.is_leftshift = False
+				return ':'
+			else:
+				return '.'
+		elif keycode == "KEY_COMMA":
+			if self.is_leftshift:
+				self.is_leftshift = False
+				return ';'
+			else:
+				return ','
+		else:
+			# Cannot handle this
+			return keycode
 
 
 
@@ -19,6 +167,12 @@ class KeyboardLibrary:
 	logging.basicConfig(filename='multik.log',level=logging.INFO)
 
 	total_keyboards = 0
+
+	keypress = event.Event('A key has been pressed')
+
+	keyboard_paths = []
+
+	keyboard_local_global_id = {}
 
 	def __init__(self):
 		# Search eventXX that matches connected keyboards
@@ -30,18 +184,26 @@ class KeyboardLibrary:
 		INPUT_EVENT_PATH = "/dev/input/"
 
 		# Create keyboard path
-		keyboard_paths = []
 
-		for ke in keyboard_events:
-			keyboard_paths.append(INPUT_EVENT_PATH + ke)
+		for counter, ke in enumerate(keyboard_events):
+			self.keyboard_paths.append(INPUT_EVENT_PATH + ke)
+			self.keyboard_local_global_id[ke[ke.find("event")+5:]] = counter
 
-		self.total_keyboards = keyboard_paths.count()
+		# Eliminamos teclado del sistema
 
+		
+
+		self.total_keyboards = len(self.keyboard_paths)-1
 	
 	def run(self):
-		for i in keyboard_paths:
-			# Automagically added to asyncore.loop map when creating this file_dispatcher
-			InputDeviceDispatcher(InputDevice(i))
 
-			# Using asyncore
-			loop()
+		for i in self.keyboard_paths:
+			# Automagically added to asyncore.loop map when creating this file_dispatcher
+			InputDeviceDispatcher(InputDevice(i)).keypress += self.Keyboard_Event
+
+		# Using asyncore
+		loop()
+
+	def Keyboard_Event(self,sender,eargs):
+		values = {"id": self.keyboard_local_global_id[eargs['id']], "char": eargs['char'], "time_pressed": eargs['time_pressed']}
+		self.keypress(values)

6c1347f9d7c06ee3ce372806d8784410c067b5bf
Andrea Vasquez G <avasquezg@gmail.com>
2014-08-05 16:22:13 -0400

    Scaffold microcuentos

---
+++ Microcuentos/microcuentos.py
@@ -0,0 +1,13 @@
+# Flujo principal
+
+# Pedir cantidad de jugadores
+
+# Hasta que todos envien X
+
+	# Mostrar: Turno jugador i iteración j
+	# Mostrar: Jugador i recibe "string" de jugador k
+
+# Cuento 1: ---
+# Cuento 2: --
+# ....
+# Cuento K 
